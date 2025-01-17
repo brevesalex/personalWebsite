ae5b1c4dd77773bd559bdc07217ffab2601e6feb
Alex Breves <abreves@yahoo.com>
2025-01-17 01:59:04 -0800

    Adding contractors under experience

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,11 @@ function openPage(pageId) {
   pages.forEach(page => {
     page.classList.remove('active');
   });
+
   document.getElementById(pageId).classList.add('active');
 
   setTimeout(function() {
     window.scrollTo(0,0);
   },0);
+  
 }
